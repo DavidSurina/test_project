e67499dae8fd193556dc07cb87d0d900a8127953
DavePersonal <david.surina22@gmail.com>
2023-01-22 23:43:12 +0100

    added outlet to navbar

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -1,24 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function Navbar(): JSX.Element {
   return (
-    <nav className="flex flex-row justify-start p-2 border-b-2 border-gray-900 sticky top-0">
-      <Link
-        to="mock1"
-        aria-label="link-mock1"
-        className="border-r-2 border-gray-900 p-2 hover:text-gray-600"
-      >
-        Mock 1
-      </Link>
-      <Link
-        to="mock2"
-        aria-label="link-mock2"
-        className="border-r-2 border-gray-900 p-2 hover:text-gray-600"
-      >
-        Mock 2
-      </Link>
-    </nav>
+    <>
+      <nav className="flex flex-row justify-start p-2 border-b-2 border-gray-900 sticky top-0">
+        <Link
+          to="mock1"
+          aria-label="link-mock1"
+          className="border-r-2 border-gray-900 p-2 hover:text-gray-600"
+        >
+          Mock 1
+        </Link>
+        <Link
+          to="mock2"
+          aria-label="link-mock2"
+          className="border-r-2 border-gray-900 p-2 hover:text-gray-600"
+        >
+          Mock 2
+        </Link>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
