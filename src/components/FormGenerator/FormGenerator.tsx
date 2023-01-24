524b43476b1b3d0d0faa4d0647e67081762251be
DavePersonal <david.surina22@gmail.com>
2023-01-24 21:15:05 +0100

    added logic to formGenerator

--- src/components/FormGenerator/FormGenerator.tsx
+++ src/components/FormGenerator/FormGenerator.tsx
@@ -1,6 +1,6 @@
-import React, { SyntheticEvent } from "react";
-import { DataObjectType } from "../../mockObjects/mockObjects";
-import EmailField from "./EmailField/EmailField";
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import { DataObjectType } from "../../globals/mockObjects";
+import InputField from "./InputField/InputField";
 
 type PropTypes = {
   dataObject: DataObjectType;
@@ -9,21 +9,45 @@ type PropTypes = {
 function FormGenerator(props: PropTypes): JSX.Element {
   const { dataObject } = props;
   const { formFields, submitBtnLabel } = dataObject;
+  const [fields, setFields] = useState(formFields);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   function onSumbit(e: SyntheticEvent): void {
     e.preventDefault();
-    console.log("submit pressed");
+    console.log("submitted");
   }
+
+  useEffect(() => {
+    const isNoError = fields.filter(
+      (field) => field.errors.length !== 0 && field.value.length !== 0
+    );
+    const isAllValid = fields.filter((field) => field.isValid === true);
+    console.log("triggered", isNoError.length, isAllValid.length, isDisabled);
+    if (isNoError.length === 0 && isAllValid.length === fields.length) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [fields]);
 
   return (
     <form onSubmit={onSumbit}>
       <>
-        {formFields.map((field) => {
-          return <EmailField field={field} />;
+        {fields.map((field, index) => {
+          return (
+            <InputField
+              field={field}
+              fields={fields}
+              setFields={setFields}
+              index={index}
+              key={index}
+            />
+          );
         })}
         <button
           type="submit"
-          className="px-4 py-2 mt-3 shadow-sm text-gray-900 self-center bg-white border-gray-900"
+          className="px-4 py-2 mt-3 shadow-sm text-gray-900 self-center bg-white border-2 border-gray-900 rounded-sm hover:shadow-lg disabled:text-gray-500 disabled:border-gray-400 disabled:bg-gray-300 disabled:hover:shadow-none"
+          disabled={isDisabled}
         >
           {submitBtnLabel}
         </button>
@@ -33,6 +57,3 @@ function FormGenerator(props: PropTypes): JSX.Element {
 }
 
 export default FormGenerator;
-
-// add some kind of error fields for validation in components
-// add validation for each type of fields - some regex field in FormFieldType
