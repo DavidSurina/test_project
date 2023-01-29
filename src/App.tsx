e512110474d410874556caf72a9a1b8ac29fdedb
DavePersonal <david.surina22@gmail.com>
2023-01-29 20:13:32 +0100

    separated imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./Layout/Navbar";
 import FormGenerator from "./components/FormGenerator/FormGenerator";
+
 import { mockObj_registration } from "./globals/mockObjects";
 
 function App() {
