8555baaac73583242b1c5d951ae5d3dd33f5a459
DavePersonal <david.surina22@gmail.com>
2023-01-29 20:08:58 +0100

    edited mockObjects

--- src/globals/mockObjects.tsx
+++ src/globals/mockObjects.tsx
@@ -1,140 +1,137 @@
 export type PatternType = {
-    pattern: string;
-    errorDesc: string;
+  pattern: string;
+  errorMessage: string;
 };
 
-export const validation: Record<string, { errorMessage: string, validate: (value: string, parameters?: unknown) => boolean }> = {
-    minLength: {
-        errorMessage: '', validate: (value, parameters) => {
-            return false
-        }
-    },
-    maxLength: {
-        errorMessage: '', validate: () => {
-            return false
-        }
-    },
-    isRequired: {
-        errorMessage: 'is required', validate: (value) => {
-            console.log(value)
-            if (!value) {
-                return false
-            }
-            return true
-        }
-    },
-    patterns: {
-        errorMessage: '', validate: () => {
-            return false
-        }
-    },
+export function patternValidate(pattern: string, value: string) {
+  const regExp = new RegExp(pattern);
+  console.log(regExp.test(value), regExp, value);
+  return regExp.test(value);
 }
+
+export const validation: Record<
+  string,
+  {
+    errorMessage: string;
+    validate: (
+      value: string | number,
+      parameters?: unknown
+    ) => boolean | typeof patternValidate;
+  }
+> = {
+  minLength: {
+    errorMessage: "Is too short",
+    validate: (value, parameters) => {
+      if ((value as string).length < (parameters as number)) {
+        return false;
+      }
+      return true;
+    },
+  },
+  maxLength: {
+    errorMessage: "Is too long",
+    validate: (value, parameters) => {
+      if ((parameters as number) < (value as string).length) {
+        return false;
+      }
+      return true;
+    },
+  },
+  isRequired: {
+    errorMessage: "Is required",
+    validate: (value) => {
+      if (!value) {
+        return false;
+      }
+      return true;
+    },
+  },
+};
 
 export type ValidationRulesType = {
-    minLength?: number;
-    maxLength?: number;
-    isRequired?: true;
-    patterns?: Array<PatternType>;
-}
+  minLength?: number;
+  maxLength?: number;
+  isRequired?: true;
+  patterns?: PatternType[];
+};
 
 export type FormFieldType = {
-    name: string;
-    label: string;
-    fieldType: "text" | "email" | "password" | "password_repeat";
-    validationRules: ValidationRulesType
-
+  name: string;
+  label: string;
+  fieldType: "text" | "email" | "password" | "password_repeat";
+  validationRules: ValidationRulesType;
 };
 
 export type DataObjectType = {
-    formFields: Array<FormFieldType>;
-    submitBtnLabel: string;
+  formFields: FormFieldType[];
+  submitBtnLabel: string;
 };
 
 const mockPw: FormFieldType = {
-    name: "password",
-    label: "Password",
-    fieldType: "password",
-    validationRules: {
-        minLength: 8,
-        maxLength: 20,
-        isRequired: true,
-        patterns: [
-            {
-                pattern: "[a-z]",
-                errorDesc: "Must contain lower case character",
-            },
-            {
-                pattern: "[A-Z]",
-                errorDesc: "Must contain upper case character",
-            },
-            {
-                pattern: ".*[0-9].*",
-                errorDesc: "Must contain number",
-            },
-            {
-                pattern: "[^A-Za-z0-9s]",
-                errorDesc: "Must contain special character",
-            },
-        ],
-    }
+  name: "password",
+  label: "Password",
+  fieldType: "password",
+  validationRules: {
+    minLength: 8,
+    maxLength: 20,
+    isRequired: true,
+    patterns: [
+      {
+        pattern: "[a-z]",
+        errorMessage: "Must contain lower case character",
+      },
+      {
+        pattern: "[A-Z]",
+        errorMessage: "Must contain upper case character",
+      },
+      {
+        pattern: ".*[0-9].*",
+        errorMessage: "Must contain number",
+      },
+      {
+        pattern: "[^A-Za-z0-9s]",
+        errorMessage: "Must contain special character",
+      },
+    ],
+  },
 };
-//
-// const mockPw_repeat: FormFieldType = {
-//   id: "213241241mnxbczkjdqasoihn12jk3",
-//   label: "Password repeat",
-//   value: "",
-//   fieldType: "password_repeat",
-//   minLength: 8,
-//   maxLength: 20,
-//   isRequired: true,
-//   isValid: false,
-//   patterns: [
-//     {
-//       pattern: "",
-//       errorDesc: "",
-//     },
-//   ],
-//   errors: [],
-// };
-//
-// const mockEmail: FormFieldType = {
-//   id: "2312312dasdaklsjdeqowpeiqoepuu414",
-//   label: "Email",
-//   value: "",
-//   fieldType: "email",
-//   minLength: 8,
-//   maxLength: 40,
-//   isRequired: true,
-//   isValid: false,
-//   patterns: [
-//     {
-//       pattern:
-//         "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
-//       errorDesc: "Field must be in email format e.g.'test@test.com'",
-//     },
-//   ],
-//   errors: [],
-// };
-//
-// const mockName: FormFieldType = {
-//   id: "31231dnzx,mcnsklejqe12op311op23j1o2p3",
-//   label: "Name",
-//   value: "",
-//   fieldType: "text",
-//   minLength: 5,
-//   maxLength: 25,
-//   isRequired: true,
-//   isValid: false,
-//   patterns: [
-//     {
-//       pattern: "^[a-zA-z]+([s][a-zA-Z]+)*$",
-//       errorDesc: "Field can only contain letters",
-//     },
-//   ],
-//   errors: [],
-// };
+
+const mockEmail: FormFieldType = {
+  name: "email",
+  label: "Email",
+  fieldType: "email",
+  validationRules: {
+    minLength: 8,
+    maxLength: 40,
+    isRequired: true,
+    patterns: [
+      {
+        pattern:
+          "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+        errorMessage: "Field should be in email format eg.test@email.com",
+      },
+    ],
+  },
+};
+
+const mockName: FormFieldType = {
+  name: "name",
+  label: "Name",
+  fieldType: "text",
+  validationRules: {
+    minLength: 5,
+    maxLength: 25,
+    isRequired: true,
+    patterns: [
+      {
+        pattern: "^[a-zA-z]+([s][a-zA-Z]+)*$",
+        errorMessage: "Field can only contain letters",
+      },
+    ],
+  },
+};
 
 export const mockObj_registration: DataObjectType = {
-    formFields: [mockPw],
-    submitBtnLabel: "Registration",
+  formFields: [mockEmail, mockName, mockPw],
+  submitBtnLabel: "Registration",
 };
