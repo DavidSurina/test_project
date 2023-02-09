b93db0c9438facb0ce0d4ced0d131b995b6c9f71
DavePersonal <david.surina22@gmail.com>
2023-02-09 21:45:02 +0100

    added correct states to useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,19 +1,27 @@
 import {useState, ChangeEvent, FormEvent} from 'react'
 import { ErrorsType, WasFieldTouchedType } from '../components/FormGenerator/types';
+import { DataObjectType } from '../globals/mockObjects';
 
 export type UseFormProps<T> = {
-    initialValues: T,
+    dataObject: DataObjectType,
     onSumbit?: (values: T) => void
 }
 
-const useForm = <T>({initialValues, onSumbit}: UseFormProps<T>) => {
-    const [values, setValues] = useState(initialValues)
+const useForm = <T>({dataObject, onSumbit}: UseFormProps<T>) => {
+    const { formFields } = dataObject;
+    const requiredFieldsCount = formFields.map((f) => f.validationRules.isRequired).length;
+    const [values, setValues] = useState({});
     const [errors, setErrors] = useState<ErrorsType>({});
-    const [wasFieldTouched, setWasFieldTouched] = useState<WasFieldTouchedType>({});  
+    const [wasFieldTouched, setWasFieldTouched] = useState<WasFieldTouchedType>({});
+    const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const {name, value} = e.target;
         setValues(prev => ({...prev, [name]: value}))
+    }
+
+    const validateField = (name: string) => {
+                
     }
 
     const handleSumbit = (e: FormEvent) => {
@@ -24,10 +32,13 @@ const useForm = <T>({initialValues, onSumbit}: UseFormProps<T>) => {
         if (onSumbit) {
             onSumbit(values)
         }
+    }   
 
+    const handleBlur = (e: FormEvent) => {
+        const { name, value } = e.currentTarget;
     }
 
-    return {handleChange, handleSumbit, values, errors}
+    return {handleChange, handleSumbit, values, errors, wasFieldTouched}
 };
 
 
