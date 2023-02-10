7b7a43e70510bc3e32227c3a36e78e0192192813
DavePersonal <david.surina22@gmail.com>
2023-02-10 21:46:55 +0100

    moved validatePattern and validatePatterns

--- src/globals/functions.tsx
+++ src/globals/functions.tsx
@@ -1,1 +1,20 @@
-export function validate() {}
+import { PatternType } from "./mockObjects";
+
+export function validatePatterns(value: string, patterns: PatternType[]) {
+  let patternErrors: string[] = [];
+
+  patterns.forEach((r) => {
+    if (validatePattern(r.pattern, value)) {
+      patternErrors = [...patternErrors, r.errorMessage];
+    }
+  });
+
+  return patternErrors;
+}
+
+export function validatePattern(pattern: string, value: string) {
+  const regExp = new RegExp(pattern);
+  console.log(regExp.test(value), regExp, value);
+
+  return regExp.test(value);
+}
