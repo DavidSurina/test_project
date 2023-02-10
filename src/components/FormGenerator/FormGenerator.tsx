3a96d1c3e5ddc53673533d1c099a47d29c98f408
DavePersonal <david.surina22@gmail.com>
2023-02-10 21:50:52 +0100

    commented out code, changed event handlers

--- src/components/FormGenerator/FormGenerator.tsx
+++ src/components/FormGenerator/FormGenerator.tsx
@@ -1,120 +1,127 @@
-import React, { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 import InputField from "./InputField/InputField";
 
-import {
-  DataObjectType,
-  PatternType,
-  patternValidate,
-  validation,
-} from "../../globals/mockObjects";
+import { DataObjectType } from "../../globals/mockObjects";
+import Checkbox from "./Checkbox/Checkbox";
+import { WasFieldTouchedType } from "./types";
+import useForm from "../../hooks/useForm";
 
 type PropTypes = {
   dataObject: DataObjectType;
 };
 
-export type ValuesType = Record<string, string>;
-export type ErrorsType = Record<string, Array<string>>;
-export type WasFieldUsedType = Record<string, boolean>;
-
 function FormGenerator(props: PropTypes): JSX.Element {
   const { dataObject } = props;
   const { formFields: fields, submitBtnLabel } = dataObject;
 
-  const [values, setValues] = useState<ValuesType>({});
-  const [errors, setErrors] = useState<ErrorsType>({});
-  const [wasFieldUsed, setWasFieldUsed] = useState<WasFieldUsedType>({});
   const [isDisabled, setIsDisabled] = useState(true);
 
+  const { handleChange, handleSumbit, values, errors, wasFieldTouched } =
+    useForm({
+      dataObject,
+    });
+
   // creates an object for every required field
-  useEffect(() => {
-    const requiredFields: WasFieldUsedType = {};
+  // useEffect(() => {
+  //   const requiredFields: WasFieldTouchedType = {};
 
-    fields.forEach((field) => {
-      if (field.validationRules.isRequired) {
-        requiredFields[field.name] = false;
-      }
-    });
+  //   fields.forEach((field) => {
+  //     if (field.validationRules.isRequired) {
+  //       console.log("triggered");
+  //       requiredFields[field.name] = false;
+  //     }
+  //   });
+  //   setWasFieldUsed(requiredFields);
+  // }, []);
 
-    setWasFieldUsed(requiredFields);
-  }, []);
+  // // checks if submit should be disabled
+  // useEffect(() => {
+  //   const wasEveryFieldUsed =
+  //     Object.keys(wasFieldUsed).length === 0 &&
+  //     Object.values(wasFieldUsed).every((v) => v);
+  //   const isErrorEmpty =
+  //     Object.keys(errors).length > 0 &&
+  //     Object.values(errors).every((item) => item.length === 0);
 
-  // checks if submit should be disabled
-  useEffect(() => {
-    const wasEveryFieldUsed = Object.values(wasFieldUsed).every(
-      (v) => v === true
-    );
-    if (
-      wasEveryFieldUsed &&
-      Object.values(errors).every((item) => item.length === 0)
-    ) {
-      setIsDisabled(false);
-    } else if (
-      isDisabled === false &&
-      !Object.values(errors).every((item) => item.length === 0)
-    ) {
-      setIsDisabled(true);
-    }
-  }, [errors]);
+  //   if (wasEveryFieldUsed && isErrorEmpty) {
+  //     setIsDisabled(false);
+  //   } else if (
+  //     !isDisabled &&
+  //     !Object.values(errors).every((item) => item.length === 0)
+  //   ) {
+  //     setIsDisabled(true);
+  //   }
+  // }, [wasFieldUsed, errors]);
 
-  function onSumbit(e: SyntheticEvent): void {
-    e.preventDefault();
-    console.log("submitted");
-  }
+  // function onSumbit(e: SyntheticEvent): void {
+  //   e.preventDefault();
+  //   console.log("submitted");
+  // }
 
-  const handleInputChange = (name: string, value: string) => {
-    setValues((prev) => ({ ...prev, [name]: value }));
-  };
+  // const handleInputChange = (name: string, value: string) => {
+  //   setValues((prev) => ({ ...prev, [name]: value }));
+  // };
 
-  const handleBlur = (name: string) => {
-    if (errors.name) {
-      setErrors((prev) => ({ ...prev, [name]: [] }));
-    }
-    let inputErrors: Array<string> = [];
-    const field = fields.find((item) => item.name === name);
+  // const handleBlur = (name: string) => {
+  //   if (errors.name) {
+  //     setErrors((prev) => ({ ...prev, [name]: [] }));
+  //   }
+  //   let inputErrors: Array<string> = [];
+  //   const field = fields.find((item) => item.name === name);
 
-    if (!field) return;
+  //   if (!field || !values[name]) return;
 
-    // if the field is password repeat - check for same value in password field
-    if (field.name === "password_repeat") {
-      const pwInput = values["password"];
-      if (
-        typeof pwInput === "string" &&
-        pwInput !== values["password_repeat"]
-      ) {
-        inputErrors = [...inputErrors, "Field does not have same value."];
-      }
-    }
+  //   // if the field is password repeat - check for same value in password field
+  //   if (field.name === "password_repeat") {
+  //     const pwInput = values["password"];
+  //     if (
+  //       typeof pwInput === "string" &&
+  //       pwInput !== values["password_repeat"]
+  //     ) {
+  //       inputErrors = [...inputErrors, "Field does not have same value."];
+  //     }
+  //   }
 
-    Object.entries(field.validationRules).forEach(([ruleKey, rule]) => {
-      if (ruleKey === "patterns") {
-        (rule as PatternType[]).forEach((r) => {
-          if (!patternValidate(r.pattern, values[name])) {
-            inputErrors = [...inputErrors, r.errorMessage];
-          }
-        });
-      } else {
-        const { validate, errorMessage } = validation[ruleKey];
-        if (!validate(values[name], rule)) {
-          inputErrors = [...inputErrors, errorMessage];
-        }
-      }
-    });
+  //   Object.entries(field.validationRules).forEach(([ruleKey, rule]) => {
+  //     if (ruleKey === "patterns") {
+  //       (rule as PatternType[]).forEach((r) => {
+  //         if (!validatePattern(r.pattern, values[name])) {
+  //           inputErrors = [...inputErrors, r.errorMessage];
+  //         }
+  //       });
+  //     } else {
+  //       const { validate, errorMessage } = validation[ruleKey];
+  //       if (!validate(values[name], rule)) {
+  //         inputErrors = [...inputErrors, errorMessage];
+  //       }
+  //     }
+  //   });
 
-    setErrors((prev) => ({ ...prev, [name]: inputErrors }));
-  };
+  //   setErrors((prev) => ({ ...prev, [name]: inputErrors }));
+  //   setWasFieldUsed((prev) => ({ ...prev, [name]: true }));
+  // };
 
   return (
-    <form onSubmit={onSumbit}>
+    <form onSubmit={handleSumbit}>
       <>
         {fields.map((field, index) => {
+          if (field.fieldType === "checkbox") {
+            return (
+              <Checkbox
+                field={field}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
+                errors={errors[field.name]}
+                key={index}
+                value={values[field.name]}
+              />
+            );
+          }
           return (
             <InputField
               field={field}
               onBlur={() => handleBlur(field.name)}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                handleInputChange(field.name, e.target.value)
-              }
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
               errors={errors[field.name]}
               key={index}
               value={values[field.name]}
